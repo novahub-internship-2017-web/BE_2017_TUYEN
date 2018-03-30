b35db08658085c3d28d286efbfdc8d40a375743e
thanhtuyenit <nguyenthithanhtuyen.dr@gmail.com>
2018-03-30 11:13:16 +0700

    springboot

---
+++ SpringGettingStarted/src/main/java/com/tuyen/demo/SpringGettingStartedApplication.java
@@ -0,0 +1,12 @@
+package com.tuyen.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringGettingStartedApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringGettingStartedApplication.class, args);
+	}
+}
